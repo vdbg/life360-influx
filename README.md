f8d2a994de438cafcd13d329009fdf27b7f9ff99
Grégory Vandenbrouck <vdb_g@hotmail.com>
2022-12-30 17:47:06 -0800

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # life360-influx
-Import life360 geodata to influx
+
+Allows for importing data from [Life360](https://www.life360.com/) to [InfluxDB](https://www.influxdata.com/) v2.
+
+
+## Requirements
+
+- [Life360](https://www.life360.com/) credentials associated with an active circle.
+- A device with either [Docker](https://www.docker.com/) or Python 3.11+ installed.
+- [InfluxDB](https://en.wikipedia.org/wiki/InfluxDB) v2 installed on this device or another device, and a bucket created in influxDB.
+
+## Setup
+
+### With Docker
+
+Dependency: Docker installed.
+
+1. Download and run the Docker image: `sudo docker run --name life360 -v config.toml:/app/config.toml vdbg/life360-influx:latest`
+2. Copy the template config file from the image: `sudo docker cp life360:/app/template.config.toml config.toml`
+3. Edit `config.toml` by following the instructions in the file
+4. Start the container again to verify the settings are correct: `sudo docker start life360 -i`
+5. Once the settings are finalized, `Ctrl-C` to stop the container, `sudo docker container rm life360` to delete it
+6. Start the container with the final settings:
+
+```
+sudo docker run \
+  -d \
+  --name life360 \
+  -v /path_to_your/config.toml:/app/config.toml \
+  --memory=100m \
+  --pull=always \
+  --restart=always \
+  vdbg/life360-influx:latest
+```
+
+### Without Docker
+
+Dependency: Python 3.11+ and pip3 installed. `sudo apt-get install python3-pip` if missing on raspbian.
+
+1. Git clone this repository and cd into directory
+2. `cp template.config.toml config.toml`
+3. Edit file `config.toml` by following the instructions in the file
+4. `pip3 install -r requirements.txt`
+5. `python3 main.py` or `./main.py`
+
+## Grafana
+
+
+To get a dashboard similar to the below one, download the [template](grafana/dashboard.json), and replace these placeholders before creating a dashboard from the template:
+* `Name_Of_Circle`: name of the Life360 circle of interest, e.g. `Family`,
+* `Member_1` to `Member_4`: names of the circle members of interest. Delete extra dashboards if fewer than 4 members, duplicate one if more.
+
+![Grafana dashboard](grafana/dashboard.png)
---
+++ life360.py
@@ -0,0 +1,129 @@
+import datetime
+import requests
+import logging
+
+# Docs for life 360 apis: https://krconv.github.io/life360-api-docs/
+# Inspired from https://github.com/harperreed/life360-python/blob/master/life360.py
+
+
+class Member:
+    def __init__(self, conf: dict) -> None:
+        self.name = self.__add_strings(conf["firstName"], conf["lastName"])
+        location: dict = conf["location"]
+        self.latitude: float = float(location["latitude"])
+        self.longitude: float = float(location["longitude"])
+        self.start: datetime = self.__get_time(location, "startTimestamp")
+        self.end: datetime = self.__get_time(location, "endTimestamp")
+        self.address: str = self.__add_strings(location["address1"], location["address2"])
+
+    def __add_strings(self, s1: str, s2: str) -> str:
+        if not s2:
+            return s1
+        return s1 + " " + s2
+
+    def __get_time(self, data: dict, key: str) -> datetime:
+        return datetime.datetime.fromtimestamp(int(data[key]), tz=datetime.timezone.utc)
+
+    def __str__(self) -> str:
+        return f"{self.name} member at {self.address} ({self.latitude},{self.longitude}) from {self.start} to {self.end}"
+
+
+class Circle:
+    def __init__(self, conf: dict) -> None:
+        self.id = conf["id"]
+        self.name: str = conf["name"]
+        self.members: list[Member] = [Member(x) for x in conf["members"]]
+
+    def __str__(self) -> str:
+        return f"{self.name} circle ({self.id})"
+
+    def get_measurements(self) -> list:
+        return None
+
+
+class Life360Connector:
+    def __init__(self, conf: dict) -> None:
+        self.auth_token: str = conf["auth_token"]
+        self.username: str = conf["username"]
+        self.password: str = conf["password"]
+        self.base_url: str = conf["base_url"]
+        self.circles_url: str = conf["circles_url"]
+        self.circle_url: str = conf["circle_url"]
+        self.token_url: str = conf["token_url"]
+        self.circle_ids: list[str] = conf["circle_ids"]
+        self.access_token: str = None
+
+    def __get_headers(self, auth: str) -> dict:
+        return {
+            "Accept": "application/json",
+            "Authorization": auth,
+            "cache-control": "no-cache",
+        }
+
+    def __make_request(self, url, retry: bool = True):
+        try:
+            self.authenticate()
+            r = requests.get(url, headers=self.__get_headers("bearer " + self.access_token))
+            r.raise_for_status()
+            return r.json()
+        except:
+            if retry:
+                self.access_token = None
+                self.authenticate(True)
+                return self.__make_request(url, False)
+            raise
+
+    def authenticate(self, force: bool = False) -> None:
+        if self.access_token and not force:
+            return
+
+        data = {
+            "grant_type": "password",
+            "username": self.username,
+            "password": self.password,
+        }
+
+        self.access_token = None
+        try:
+            r = requests.post(self.base_url + self.token_url, data=data, headers=self.__get_headers("Basic " + self.auth_token))
+            r.raise_for_status()
+            self.access_token = r.json()["access_token"]
+        except:
+            logging.exception("Unable to authenticate")
+            raise
+
+    def get_circles(self) -> list[str]:
+        if not self.circle_ids:
+            r = self.__make_request(self.base_url + self.circles_url)
+            self.circle_ids = [x["id"] for x in r["circles"]]
+            logging.debug(f"Caching circle ids: {self.circle_ids}")
+        return self.circle_ids
+
+    def get_circle(self, circle_id) -> Circle:
+        r = self.__make_request(self.base_url + self.circle_url + circle_id)
+        return Circle(r)
+
+    def get_records(self, measurement_name: str) -> list:
+        records = []
+        for circle_id in self.get_circles():
+            logging.debug(f"Processing circleId {circle_id}")
+            circle = self.get_circle(circle_id)
+            for member in circle.members:
+                logging.debug(f"Processing member {member}")
+                for time in (member.start, member.end):
+                    records.append(
+                        {
+                            "measurement": measurement_name,
+                            "tags": {
+                                "circle": circle.name,
+                                "name": member.name,
+                            },
+                            "fields": {
+                                "latitude": member.latitude,
+                                "longitude": member.longitude,
+                                "address": member.address,
+                            },
+                            "time": time,
+                        }
+                    )
+        return records
